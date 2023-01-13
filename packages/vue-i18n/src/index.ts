8aee73d8bc5b81724f6c741bae46f9b0cda07bdc
aminevg <38422328+aminevg@users.noreply.github.com>
2023-01-13 20:06:23 +0900

    feat(vue-i18n): add Hybridly plugin for locale switching

--- packages/vue-i18n/src/index.ts
+++ packages/vue-i18n/src/index.ts
@@ -1,3 +1,20 @@
-export function testFunction (name: string) {
-  return `Hello ${name}`
+import { Composer, I18n, VueI18n } from "@intlify/vue-i18n-core";
+import { definePlugin } from "@hybridly/core";
+import { useTypedProperty } from "@hybridly/vue";
+
+export function hybridlyLocaleSwitcher(i18nInstance: I18n) {
+  return definePlugin({
+    name: "hybridly-locale-switcher:set-locale",
+    navigated() {
+      const currentLocale = useTypedProperty<string>(
+        "hybridlyLocaleSwitcher.currentLocale"
+      );
+
+      if (i18nInstance.mode === "legacy") {
+        (i18nInstance.global as VueI18n).locale = currentLocale.value;
+      } else {
+        (i18nInstance.global as Composer).locale.value = currentLocale.value;
+      }
+    },
+  });
 }
