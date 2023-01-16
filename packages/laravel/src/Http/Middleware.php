8d88423e0c807a8fc9d12d42c1aafa7f8cdc6ffa
aminevg <38422328+aminevg@users.noreply.github.com>
2023-01-17 00:12:49 +0900

    fix: return type is Symfony response

--- packages/laravel/src/Http/Middleware.php
+++ packages/laravel/src/Http/Middleware.php
@@ -4,7 +4,7 @@ namespace Aminevg\HybridlyLocaleSwitcher\Http;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 
 class Middleware
 {
