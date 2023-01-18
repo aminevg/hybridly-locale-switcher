99d9fa66c2bf217b5544d3f5aec7ad5d905f363c
aminevg <38422328+aminevg@users.noreply.github.com>
2023-01-19 00:46:28 +0900

    fix(vue-i18n): fix Vue-side plugin

--- packages/vue-i18n/src/index.ts
+++ packages/vue-i18n/src/index.ts
@@ -1,24 +1,27 @@
-import { Composer, I18n, VueI18n } from "@intlify/vue-i18n-core";
-import { definePlugin } from "@hybridly/core";
-import { useTypedProperty } from "@hybridly/vue";
+import { Composer, I18n, VueI18n } from "vue-i18n";
+import { definePlugin, Plugin } from "hybridly";
 
-export function hybridlyLocaleSwitcher(options: HybridlyLocaleSwitcherOptions) {
+export function hybridlyLocaleSwitcher(
+  options: HybridlyLocaleSwitcherOptions
+): Plugin {
   return definePlugin({
     name: "hybridly-locale-switcher:set-locale",
-    navigated() {
-      const currentLocale = useTypedProperty<string>(
-        "hybridlyLocaleSwitcher.currentLocale"
-      );
+    navigated(_, context) {
+      const props = context.view.properties.hybridlyLocaleSwitcher;
+      if (typeof props !== "object" || props === null || Array.isArray(props))
+        return;
+      if (typeof props.currentLocale !== "string") return;
 
       if (options.i18nInstance.mode === "legacy") {
-        (options.i18nInstance.global as VueI18n).locale = currentLocale.value;
+        (options.i18nInstance.global as VueI18n).locale = props.currentLocale;
       } else {
-        (options.i18nInstance.global as Composer).locale.value = currentLocale.value;
+        (options.i18nInstance.global as Composer).locale.value =
+          props.currentLocale;
       }
     },
   });
 }
 
 interface HybridlyLocaleSwitcherOptions {
-  i18nInstance: I18n
+  i18nInstance: I18n;
 }
